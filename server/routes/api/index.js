e2818d7609816d41e5e1a523f08cb552c3444a69
galang pham <phamgalang@gmail.com>
2017-08-04 11:51:06 -0700

    started user routes, updated readme

---
+++ server/routes/api/index.js
@@ -0,0 +1,20 @@
+const router = require('express').Router();
+
+router.use('/', require('./users'));
+
+//error route handler to convert mongoose validation errors so the front end can consume otherwise would return 500 internal server error
+router.use(function(err, req, res, next) {
+  if(err.name === 'ValidationError'){
+    return res.status(422).json({
+      errors: Object.keys(err.errors).reduce(function(errors, key) {
+        errors[key] = err.errors[key].message;
+
+        return errors;
+      }, {})
+    });
+  }
+
+  return next(err)
+})
+
+module.exports = router;
---
+++ server/routes/api/users.js
@@ -0,0 +1,88 @@
+'use strict';
+
+const mongoose = require('mongoose');
+const router = require('express').Router();
+const passport = require('passport');
+const User = mongoose.model('User');
+const auth = require('../auth');
+
+//route to create new users
+router.post('/users', function(req, res, next) {
+  const user = new User();
+
+  user.firstName = req.body.user.firstName;
+  user.lastName = req.body.user.lastName;
+  user.email = req.body.user.email;
+  user.work = req.body.user.work;
+  user.position = req.body.user.position;
+  user.phone = req.body.user.phone;
+
+  user.save().then(function() {
+    return res.json({user: user.toAuthJSON()});
+  }).catch(next);
+});
+
+router.post('/users/login', function(req, res, next) {
+  if(!req.body.user.email) {
+    return res.status(422).json({errors: {email: "can't be blank"}});
+  }
+
+  if(!req.body.user.password){
+   return res.status(422).json({errors: {password: "can't be blank"}});
+ }
+
+//use local strategy as defined in config for authentication. session:false to prevent Passport from serializing user info in the sesion. Callback will respond to client based off if authentication was successful or not
+  passport.authenticate('local', {session: false}, function(err, user, info) {
+    if(err) { return next(err); }
+
+    if(user){
+      user.token = user.generateJWT();
+      return res.json({ user: user.toAuthJSON()});
+    } else {
+      return res.status(422).json(info)
+    }
+  })(req, res, next);
+});
+
+router.get('/user', auth.required, function(req, res, next) {
+  User.findById(req.payload.id).then(function(user) {
+    if(!user) { return res.sendStatus(401); }
+
+    return res.json({user: user.toAuthJSON()});
+  }).catch(next);
+});
+
+router.put('/user', auth.required, function(req, res, next){
+    User.findById(req.payload.id).then(function(user) {
+      if(!user) { return res.sendStatus(401); }
+
+      // only update fields that were actually passed...
+    if(typeof req.body.user.firstName !== 'undefined'){
+      user.firstName = req.body.user.firstName;
+    }
+    if(typeof req.body.user.lastName !== 'undefined'){
+      user.lastName = req.body.user.lastName;
+    }
+    if(typeof req.body.user.email !== 'undefined'){
+      user.email = req.body.user.email;
+    }
+    if(typeof req.body.user.work !== 'undefined'){
+      user.work = req.body.user.work;
+    }
+    if(typeof req.body.user.position !== 'undefined'){
+      user.position = req.body.user.position;
+    }
+    if(typeof req.body.user.phone !== 'undefined'){
+      user.phone = req.body.user.phone;
+    }
+    if(typeof req.body.user.password !== 'undefined'){
+      user.setPassword(req.body.user.password);
+    }
+
+    return user.save().then(function(){
+      return res.json({user: user.toAuthJSON()});
+    });
+  }).catch(next);
+});
+
+module.exports = router;
